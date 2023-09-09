2acc61e4c772310f4525f4ffffb1172616a6c8cf
mikeqiu2011 <mikeqiu2011@163.com>
2023-09-09 19:53:16 +0800

    partial function for multiple argument for map

--- multiprocessing_pool.py
+++ multiprocessing_pool.py
@@ -1,10 +1,14 @@
 import time
 from multiprocessing import Pool, cpu_count
+from functools import partial
 
 
-def square(x):
-    return x ** 2
+def square(y, x):
+    return x ** y
 
+
+power = 3
+partial_func = partial(square, power)
 
 if __name__ == '__main__':
     num_process = cpu_count()
@@ -14,7 +18,7 @@ if __name__ == '__main__':
     start = time.time()
 
     with Pool(num_process) as pool:
-        result = pool.map(square, li)
+        result = pool.map(partial_func, li)
 
     print(result)
 

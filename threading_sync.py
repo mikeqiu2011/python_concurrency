8d5fa207617156bee58b7d9889b76c3c7b22335b
mikeqiu2011 <mikeqiu2011@163.com>
2023-09-09 14:29:38 +0800

    lock

--- threading_sync.py
+++ threading_sync.py
@@ -4,15 +4,19 @@ import time
 x = 0
 
 
-def thread_task():
+def thread_task(lock):
     global x
-    for i in range(10):
+    for i in range(100000):
+        lock.acquire()
         x += 1
+        lock.release()
 
 
 if __name__ == '__main__':
-    t1 = threading.Thread(target=thread_task)
-    t2 = threading.Thread(target=thread_task)
+    lock = threading.Lock()
+
+    t1 = threading.Thread(target=thread_task, args=(lock,))
+    t2 = threading.Thread(target=thread_task, args=(lock, ))
 
     t1.start()
     t2.start()

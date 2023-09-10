3da6b006f28d39495ba8091c69f341c78d3a5398
mikeqiu2011 <mikeqiu2011@163.com>
2023-09-10 08:48:07 +0800

    async wait, return when all completed

--- async/async_wait.py
+++ async/async_wait.py
@@ -12,10 +12,12 @@ async def main():
         pending.add(asyncio.create_task(async_sleep(i+1)))
 
     while len(pending) > 0:
-        done, pending = await asyncio.wait(pending, timeout=2)
+        done, pending = await asyncio.wait(pending, return_when='ALL_COMPLETED')
         print('len done:', len(done))
         for done_task in done:
             print(await done_task)
+
+        # pending.add(asyncio.create_task(async_sleep(1)))
 
         print('len pending:', len(pending))
         print('*' * 10)

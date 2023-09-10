9f59fb8dbaabd4db1a59dd6ea5597c992b8415d9
mikeqiu2011 <mikeqiu2011@163.com>
2023-09-10 08:45:43 +0800

    async wait process in chunk

--- async/async_wait.py
+++ async/async_wait.py
@@ -1,14 +1,24 @@
 import asyncio
 
 
-async def main():
-    tasks = set()
-    for i in range(10):
-        tasks.add(asyncio.create_task(asyncio.sleep(i+1)))
+async def async_sleep(duration):
+    await asyncio.sleep(duration)
+    return duration
 
-    done, pending = await asyncio.wait(tasks)
-    print(done)
-    print(pending)
+
+async def main():
+    pending = set()
+    for i in range(10):
+        pending.add(asyncio.create_task(async_sleep(i+1)))
+
+    while len(pending) > 0:
+        done, pending = await asyncio.wait(pending, timeout=2)
+        print('len done:', len(done))
+        for done_task in done:
+            print(await done_task)
+
+        print('len pending:', len(pending))
+        print('*' * 10)
 
 
 if __name__ == '__main__':
